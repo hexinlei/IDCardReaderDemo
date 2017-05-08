c87cf60c9dc93711f4892914b4b1aa8365dcde7d
wangyunlei <wangyunlei@markor.com.cn>
2017-05-09 01:47:28 +0800

    下载上传信息接口

---
+++ app/src/main/java/com/demo/ranger/idreaderdemo/data/PostCardInfoData.java
@@ -0,0 +1,522 @@
+package com.demo.ranger.idreaderdemo.data;
+
+/**
+ * @project:IDCardReaderDemo
+ * @package:com.demo.ranger.idreaderdemo.data
+ * @copyright: Copyright[2017-2999] [Markor Investment Group Co. LTD]. All Rights Reserved.
+ * @filename: PostCardInfoData
+ * @description:&lt;描述&gt;
+ * @author: wangyunlei
+ * @date: 17/5/8-下午11:25
+ * @version: 1.0
+ */
+public class PostCardInfoData
+{
+	/**
+	 * token : XXXXXXXXXXXXXX
+	 * deviceCode : 650105FL000001000001
+	 * blackQueryJobId : 0
+	 * collectionType : 1
+	 * passNo : 00111333
+	 * inOrOut : 0
+	 * verifyResult : 0
+	 * passTime : 2016-03-08 16:25:38
+	 * name : XXXX
+	 * genderCode : 1
+	 * nationCode : 01
+	 * nationality : 中国
+	 * birthday : 19890924
+	 * addr : 黑龙江省汤原县
+	 * cardNo : 2308281989xxxxxxxx
+	 * cardStartTime : 20151212
+	 * cardEndTime : 20351212
+	 * cardType : 1
+	 * issuingUnit : ABC公安局
+	 * hasFingerFea : 1
+	 * fingerFeature0 : base64
+	 * fingerFeature1 : base64
+	 * faceCompareResult : 1
+	 * faceQualityScore : 99.99
+	 * faceCompareScore : 998.96
+	 * cardImgPath : image/192122354883355.jpg
+	 * sceneImgPath : image/192122354883355.jpg
+	 * faceImgPath : image/1924515743125.jpg
+	 * fingerImgPath : image/11237862878373.jpg
+	 * fingerCompareResult : 1
+	 * fingerCompareFeatureId : 1
+	 * fingerQualityScore : 88.35
+	 * fingerCompareScore : 999.99
+	 * hasTicket : 0
+	 * stationNo : 650105001
+	 * ticketNo : 321450
+	 * ticketType : 1
+	 * trainNo : Z168
+	 * goTime : 2016-03-07 16:18:00
+	 * goAddr : 北京
+	 * arriveTime : 2016-03-07 16:18:00
+	 * arriveAddr : 新加坡
+	 */
+
+	private String token;
+	private String deviceCode;
+	private String blackQueryJobId;
+	private int collectionType;
+	private String passNo;
+	private int inOrOut;
+	private int verifyResult;
+	private String passTime;
+	private String name;
+	private String genderCode;
+	private String nationCode;
+	private String nationality;
+	private String birthday;
+	private String addr;
+	private String cardNo;
+	private String cardStartTime;
+	private String cardEndTime;
+	private int cardType;
+	private String issuingUnit;
+	private int hasFingerFea;
+	private String fingerFeature0;
+	private String fingerFeature1;
+	private int faceCompareResult;
+	private double faceQualityScore;
+	private double faceCompareScore;
+	private String cardImgPath;
+	private String sceneImgPath;
+	private String faceImgPath;
+	private String fingerImgPath;
+	private int fingerCompareResult;
+	private int fingerCompareFeatureId;
+	private double fingerQualityScore;
+	private double fingerCompareScore;
+	private int hasTicket;
+	private String stationNo;
+	private String ticketNo;
+	private int ticketType;
+	private String trainNo;
+	private String goTime;
+	private String goAddr;
+	private String arriveTime;
+	private String arriveAddr;
+
+	public String getToken()
+	{
+		return token;
+	}
+
+	public void setToken(String token)
+	{
+		this.token = token;
+	}
+
+	public String getDeviceCode()
+	{
+		return deviceCode;
+	}
+
+	public void setDeviceCode(String deviceCode)
+	{
+		this.deviceCode = deviceCode;
+	}
+
+	public String getBlackQueryJobId()
+	{
+		return blackQueryJobId;
+	}
+
+	public void setBlackQueryJobId(String blackQueryJobId)
+	{
+		this.blackQueryJobId = blackQueryJobId;
+	}
+
+	public int getCollectionType()
+	{
+		return collectionType;
+	}
+
+	public void setCollectionType(int collectionType)
+	{
+		this.collectionType = collectionType;
+	}
+
+	public String getPassNo()
+	{
+		return passNo;
+	}
+
+	public void setPassNo(String passNo)
+	{
+		this.passNo = passNo;
+	}
+
+	public int getInOrOut()
+	{
+		return inOrOut;
+	}
+
+	public void setInOrOut(int inOrOut)
+	{
+		this.inOrOut = inOrOut;
+	}
+
+	public int getVerifyResult()
+	{
+		return verifyResult;
+	}
+
+	public void setVerifyResult(int verifyResult)
+	{
+		this.verifyResult = verifyResult;
+	}
+
+	public String getPassTime()
+	{
+		return passTime;
+	}
+
+	public void setPassTime(String passTime)
+	{
+		this.passTime = passTime;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	public String getGenderCode()
+	{
+		return genderCode;
+	}
+
+	public void setGenderCode(String genderCode)
+	{
+		this.genderCode = genderCode;
+	}
+
+	public String getNationCode()
+	{
+		return nationCode;
+	}
+
+	public void setNationCode(String nationCode)
+	{
+		this.nationCode = nationCode;
+	}
+
+	public String getNationality()
+	{
+		return nationality;
+	}
+
+	public void setNationality(String nationality)
+	{
+		this.nationality = nationality;
+	}
+
+	public String getBirthday()
+	{
+		return birthday;
+	}
+
+	public void setBirthday(String birthday)
+	{
+		this.birthday = birthday;
+	}
+
+	public String getAddr()
+	{
+		return addr;
+	}
+
+	public void setAddr(String addr)
+	{
+		this.addr = addr;
+	}
+
+	public String getCardNo()
+	{
+		return cardNo;
+	}
+
+	public void setCardNo(String cardNo)
+	{
+		this.cardNo = cardNo;
+	}
+
+	public String getCardStartTime()
+	{
+		return cardStartTime;
+	}
+
+	public void setCardStartTime(String cardStartTime)
+	{
+		this.cardStartTime = cardStartTime;
+	}
+
+	public String getCardEndTime()
+	{
+		return cardEndTime;
+	}
+
+	public void setCardEndTime(String cardEndTime)
+	{
+		this.cardEndTime = cardEndTime;
+	}
+
+	public int getCardType()
+	{
+		return cardType;
+	}
+
+	public void setCardType(int cardType)
+	{
+		this.cardType = cardType;
+	}
+
+	public String getIssuingUnit()
+	{
+		return issuingUnit;
+	}
+
+	public void setIssuingUnit(String issuingUnit)
+	{
+		this.issuingUnit = issuingUnit;
+	}
+
+	public int getHasFingerFea()
+	{
+		return hasFingerFea;
+	}
+
+	public void setHasFingerFea(int hasFingerFea)
+	{
+		this.hasFingerFea = hasFingerFea;
+	}
+
+	public String getFingerFeature0()
+	{
+		return fingerFeature0;
+	}
+
+	public void setFingerFeature0(String fingerFeature0)
+	{
+		this.fingerFeature0 = fingerFeature0;
+	}
+
+	public String getFingerFeature1()
+	{
+		return fingerFeature1;
+	}
+
+	public void setFingerFeature1(String fingerFeature1)
+	{
+		this.fingerFeature1 = fingerFeature1;
+	}
+
+	public int getFaceCompareResult()
+	{
+		return faceCompareResult;
+	}
+
+	public void setFaceCompareResult(int faceCompareResult)
+	{
+		this.faceCompareResult = faceCompareResult;
+	}
+
+	public double getFaceQualityScore()
+	{
+		return faceQualityScore;
+	}
+
+	public void setFaceQualityScore(double faceQualityScore)
+	{
+		this.faceQualityScore = faceQualityScore;
+	}
+
+	public double getFaceCompareScore()
+	{
+		return faceCompareScore;
+	}
+
+	public void setFaceCompareScore(double faceCompareScore)
+	{
+		this.faceCompareScore = faceCompareScore;
+	}
+
+	public String getCardImgPath()
+	{
+		return cardImgPath;
+	}
+
+	public void setCardImgPath(String cardImgPath)
+	{
+		this.cardImgPath = cardImgPath;
+	}
+
+	public String getSceneImgPath()
+	{
+		return sceneImgPath;
+	}
+
+	public void setSceneImgPath(String sceneImgPath)
+	{
+		this.sceneImgPath = sceneImgPath;
+	}
+
+	public String getFaceImgPath()
+	{
+		return faceImgPath;
+	}
+
+	public void setFaceImgPath(String faceImgPath)
+	{
+		this.faceImgPath = faceImgPath;
+	}
+
+	public String getFingerImgPath()
+	{
+		return fingerImgPath;
+	}
+
+	public void setFingerImgPath(String fingerImgPath)
+	{
+		this.fingerImgPath = fingerImgPath;
+	}
+
+	public int getFingerCompareResult()
+	{
+		return fingerCompareResult;
+	}
+
+	public void setFingerCompareResult(int fingerCompareResult)
+	{
+		this.fingerCompareResult = fingerCompareResult;
+	}
+
+	public int getFingerCompareFeatureId()
+	{
+		return fingerCompareFeatureId;
+	}
+
+	public void setFingerCompareFeatureId(int fingerCompareFeatureId)
+	{
+		this.fingerCompareFeatureId = fingerCompareFeatureId;
+	}
+
+	public double getFingerQualityScore()
+	{
+		return fingerQualityScore;
+	}
+
+	public void setFingerQualityScore(double fingerQualityScore)
+	{
+		this.fingerQualityScore = fingerQualityScore;
+	}
+
+	public double getFingerCompareScore()
+	{
+		return fingerCompareScore;
+	}
+
+	public void setFingerCompareScore(double fingerCompareScore)
+	{
+		this.fingerCompareScore = fingerCompareScore;
+	}
+
+	public int getHasTicket()
+	{
+		return hasTicket;
+	}
+
+	public void setHasTicket(int hasTicket)
+	{
+		this.hasTicket = hasTicket;
+	}
+
+	public String getStationNo()
+	{
+		return stationNo;
+	}
+
+	public void setStationNo(String stationNo)
+	{
+		this.stationNo = stationNo;
+	}
+
+	public String getTicketNo()
+	{
+		return ticketNo;
+	}
+
+	public void setTicketNo(String ticketNo)
+	{
+		this.ticketNo = ticketNo;
+	}
+
+	public int getTicketType()
+	{
+		return ticketType;
+	}
+
+	public void setTicketType(int ticketType)
+	{
+		this.ticketType = ticketType;
+	}
+
+	public String getTrainNo()
+	{
+		return trainNo;
+	}
+
+	public void setTrainNo(String trainNo)
+	{
+		this.trainNo = trainNo;
+	}
+
+	public String getGoTime()
+	{
+		return goTime;
+	}
+
+	public void setGoTime(String goTime)
+	{
+		this.goTime = goTime;
+	}
+
+	public String getGoAddr()
+	{
+		return goAddr;
+	}
+
+	public void setGoAddr(String goAddr)
+	{
+		this.goAddr = goAddr;
+	}
+
+	public String getArriveTime()
+	{
+		return arriveTime;
+	}
+
+	public void setArriveTime(String arriveTime)
+	{
+		this.arriveTime = arriveTime;
+	}
+
+	public String getArriveAddr()
+	{
+		return arriveAddr;
+	}
+
+	public void setArriveAddr(String arriveAddr)
+	{
+		this.arriveAddr = arriveAddr;
+	}
+}
